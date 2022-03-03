36417afb18243653f0830f2e881f8faeea8952bd
isabella <bellamay54.im@gmail.com>
2022-03-03 10:01:29 -0300

    feature: routes GET and post, missing UPDATE and DELETE

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,40 @@
 from http.client import NOT_FOUND
-from flask import Flask,jsonify, request
+from flask import Flask, jsonify, request
 from http import HTTPStatus
-from .products import  data
+from .products import data, read_id, add_product
 
 app = Flask(__name__)
 
+
 @app.get('/products')
-def products():
+def allproducts():
     ...
-    return jsonify(data), HTTPStatus.OK
+    print(f'{request.args=}')
+    return jsonify(data)
+
+
+@app.get('/products/<product_id>')
+def read_id_product(product_id):
+    args = read_id(product_id)
+
+    if args == 'Not Found':
+
+        return {'error': 'product not found'}, HTTPStatus.NOT_FOUND
+    return jsonify(args), HTTPStatus.OK
+
+
+@app.post('/products')
+def create_product():
+    new_data = request.get_json()
+
+    expected = {'name', 'price'}
+    received = set(new_data.keys())
+    missing = received - expected
+
+    name = new_data.get('name')
+    price = new_data.get('price')
+
+    if missing or (name == None or price == None):
+        return {'error': 'missing  name or price'}, HTTPStatus.BAD_REQUEST
+
+    return add_product(name, price), HTTPStatus.CREATED
--- app/products/__init__.py
+++ app/products/__init__.py
@@ -1,6 +1,7 @@
 from os import getenv
 import csv
 
+# listando os produtos do csv
 path = getenv('FILEPATH')
 data = []
 f = open(path, 'r')
@@ -12,3 +13,36 @@ for line in reader:
     data.append(line)
 
 f.close()
+
+# lendo o id e comparando para retornar
+
+
+def read_id(product_id):
+    ...
+    path = getenv('FILEPATH')
+    with open(path, 'r') as arquivo_csv:
+        new_arquivo = csv.DictReader(arquivo_csv)
+        for product in new_arquivo:
+            if product["id"] == product_id:
+                product["id"] = int(product["id"])
+                f.close()
+                return product
+
+    f.close()
+    return 'Not Found'
+
+# criando outro produto
+
+
+def add_product(name, price):
+    path = getenv('FILEPATH')
+    fieldnames = ['id', 'name', 'price']
+    new_id_product = data[-1]['id'] + 1
+    payload = {'id': new_id_product, 'name': name, 'price': float(price)}
+    f = open(path, 'a')
+    writer = csv.DictWriter(f, fieldnames=fieldnames)
+    writer.writerow(payload)
+    f.close()
+    data.append(payload)
+
+    return payload
